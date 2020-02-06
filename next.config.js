88b4dd5f78c57e7cfe723408f154f20b5e5670f7
Dennis Wirya <dwirya@outlook.com>
2020-02-06 15:25:34 +0800

    Set up PurgeCSS and reading .env
    PurgeCSS is used to remove unused styles from Tailwind

--- next.config.js
+++ next.config.js
@@ -1,2 +1,23 @@
-const withImages = require('next-images')
-module.exports = withImages()
+// Reference:
+// Environment variables: https://gist.github.com/pkellner/a2362c22cfd214588b7a4450f59a0a6a
+
+require('dotenv').config();
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const withImages = require('next-images');
+
+module.exports = withImages({
+  webpack(config, options) {
+    config.plugins = config.plugins || [];
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file and attach environment variables
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ];
+    return config
+  }
+});
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
+// Reference:
+// PurgeCSS setup: https://github.com/tailwindcss/setup-examples/tree/master/examples/nextjs
+
+// In the "content" variable, the regex indicates that PurgeCSS will only prune Tailwind
+// styles which are only found in "components" and "pages" folder.
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  }
+];
+
 module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
-    "autoprefixer"
+    "autoprefixer",
+    ...(process.env.STAGE === "production" ? [purgecss] : [])
   ]
 };
