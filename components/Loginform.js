ef0275e27edbc720bb4deaec190ca5d936e14d12
Saint_Tan <tanhwaisaint@hotmail.com>
2020-02-07 10:37:57 +0800

    added a way to extract form information and login button

--- components/Loginform.js
+++ components/Loginform.js
@@ -1,17 +1,30 @@
-import React,{Component} from 'react';
+import React from 'react';
+import {useInput} from '../modhooks/useInput';
 
 export default function LoginForm(){
+  const {value:username, reset:resetUsername, bind:bindUsername}= useInput("");
+  const {value:password, reset:resetPassword, bind:bindPassword}= useInput("");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    resetUsername;
+    resetPassword;
+  }
+
   return(
     <div>
       <form>
         <label>
           Username:
-          <input type= "text"/>
+          <input
+            type= "text" {...bindUsername}/>
         </label>
         <label>
           Password:
-          <input type= "text"/>
+          <input
+            type= "Password" {...bindPassword}/>
         </label>
+        <input type= "submit" value="Login"/>
       </form>
     </div>
   )
